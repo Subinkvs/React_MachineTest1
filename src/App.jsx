fdf0aa8dad1191ef2857170e08c0c4ed793e8bd9
SubinSundar <subinsundar9624@gmail.com>
2024-06-15 14:08:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import HeaderComponent from "./Header/header";
+import ContentComponent1 from "./Content1/ContentComponent1";
+
+function App() {
+  return (
+    <>
+      <HeaderComponent></HeaderComponent>
+      <ContentComponent1></ContentComponent1>
+    </>
+  );
+}
+
+export default App;
